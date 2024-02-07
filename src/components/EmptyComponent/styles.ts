fc96a575932f232de9f82d3f2e2bb442b82b6b60
ian.vinicius <ian.vinicius@tascomtecnologia.com.br>
2024-02-07 18:48:53 -0300

    feat: typing and styling custom flatlist

--- src/components/EmptyComponent/styles.ts
+++ src/components/EmptyComponent/styles.ts
@@ -1,11 +1,11 @@
 import styled from "styled-components/native";
 
 const EmptyMessageContainer = styled.Text`
-  border-radius: ${({ theme }) => theme.shape.borderRadius};
-  padding: ${({ theme }) => theme.shape.padding};
+  border-radius: ${({ theme }) => theme.shape.borderRadius + "px"};
+  padding: ${({ theme }) => theme.shape.padding + "px"};
   background-color: ${({ theme }) => theme.colors.secondary?.[100]};
   font-family: ${({ theme }) => theme.typography.fonts.primary.normal};
-  font-size: ${({ theme }) => theme.typography.size.body};
+  font-size: ${({ theme }) => theme.typography.size.body + "px"};
   color: ${({ theme }) => theme.colors.secondary?.[600]};
 `;
 

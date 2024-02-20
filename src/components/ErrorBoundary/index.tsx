19c6b56ca4ea3ab634fafa1c0cfdfa5d190967e1
ian.vinicius <ian.vinicius@tascomtecnologia.com.br>
2024-02-20 17:30:47 -0300

    feat: typing and styling error boundary

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,27 +1,26 @@
 /* eslint-disable no-console */
 import React from "react";
-import { Text, View } from "react-native";
-import ErrorBoundary from "react-native-error-boundary";
+import ErrorBoundary, { FallbackComponentProps } from "react-native-error-boundary";
 import LottieView from "lottie-react-native";
-import styles from "./styles";
+import {
+  AnimationContainer,
+  Container,
+  EmphasisMessage,
+  ErrorViewer,
+  Message,
+  Title,
+  Wrapper,
+} from "./styles";
 import errorAnimation from "../../animations/error.json";
 import { MainButton } from "../Buttons";
+import { IErrorBoundary } from "./types";
 
-export default function CustomErrorBoundary({ children }) {
-  const errorStyles = styles();
-
-  function FallbackComponent({ error, resetError }) {
+export default function CustomErrorBoundary({ children }: IErrorBoundary) {
+  function FallbackComponent({ error, resetError }: FallbackComponentProps) {
     return (
-      <View style={errorStyles.wrapper}>
-        <View style={errorStyles.container}>
-          <View
-            style={{
-              borderRadius: 250,
-              width: 250,
-              height: 250,
-              overflow: "hidden",
-            }}
-          >
+      <Wrapper>
+        <Container>
+          <AnimationContainer>
             <LottieView
               autoPlay
               loop={false}
@@ -31,16 +30,16 @@ export default function CustomErrorBoundary({ children }) {
                 height: "100%",
               }}
             />
-          </View>
-          <Text style={errorStyles.title}>Ops, algo está errado...</Text>
-        </View>
-        <Text style={errorStyles.message}>
+          </AnimationContainer>
+          <Title>Ops, algo está errado...</Title>
+        </Container>
+        <Message>
           Ocorreu um erro interno no aplicativo, por favor, nos reporte o ocorrido assim que
-          possível e tente novamente <Text style={errorStyles.semibold}>:D</Text>
-        </Text>
+          possível e tente novamente <EmphasisMessage>:D</EmphasisMessage>
+        </Message>
         <MainButton onPress={resetError}>Tentar novamente</MainButton>
-        <Text style={errorStyles.error}>{error.toString()}</Text>
-      </View>
+        <ErrorViewer>/{error.toString()}</ErrorViewer>
+      </Wrapper>
     );
   }
 
